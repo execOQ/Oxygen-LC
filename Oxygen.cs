f7312a4dbd5ce81cfb5785f4379508779ca8d680
Artem Bagin <execoq@gmail.com>
2024-06-29 16:50:39 +0500

    v1.6.1

--- Oxygen.cs
+++ Oxygen.cs
@@ -24,7 +24,7 @@ namespace Oxygen
 
         public const string modName = "Oxygen";
         public const string modGUID = "consequential.Oxygen";
-        public const string modVersion = "1.6.0";
+        public const string modVersion = "1.6.1";
 
         private readonly Harmony harmony = new(modGUID);
         public static OxygenConfig OxygenConfig { get; private set; }
